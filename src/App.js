345d19fc8704e128826b864a622c79a4c185f102
Beimar Medina <bmedina@vitas.com>
2017-11-12 20:32:41 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Blog</h2>          
+        </header>
+        <p className="App-intro">
+          Intro ..... bla bla bla .....
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
